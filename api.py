b7832625ca61b4d347597dca3785db319a719f16
Mur_ma <90374636+Murrma@users.noreply.github.com>
2021-10-13 13:32:44 +0300

    Add files via upload

--- api.py
+++ api.py
@@ -4,22 +4,35 @@ from flask_restful import Api, Resource
 from ClientModel import Client
 from OrderModel import Order
 import requests
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["JWT_SECRET_KEY"] = "rf;kpkvbdtjm;fpkdpvfjovjcdlnlpjvfl"
 db = SQLAlchemy(app)
 api = Api(app)
+jwt = JWTManager(app)
 
 class Authorization(Resource):
-    def auth(self):
+    def login(self):
         data = requests.get_json(forse=True)
+        #data = {'username': '', 'password': ''}
+        info = Client.query.filter_by(username = data['username']).one_or_none()
 
-        return 200
+        if data['password'] != info.password or info == None:
+            return jsonify({"msg": "Bad password or username"}), 401
+        access_token = create_access_token(identity=data['username'])
+        return jsonify(access_token=access_token)
+
+    @jwt_required()
+    def logout(self):
+        pass
 
 
 class ClientApi(Resource):
     @staticmethod
+    @jwt_required()
     def get(self, id=None):
         if id is None:
             info = Client.query.all()
@@ -39,7 +52,7 @@ class ClientApi(Resource):
     @staticmethod
     def post(self):
         data = requests.get_json(forse=True)
-        #data == {f_name:..., ...}
+        #data == {'f_name':..., ...}
 
         db.session.add(Client(username = data['username'],
                               password = data['password'],
@@ -49,9 +62,10 @@ class ClientApi(Resource):
         return 200
 
     @staticmethod
+    @jwt_required()
     def patch(self, id):
         data = requests.get_json(forse=True)
-        # data == {f_name:..., ...}
+        # data == {'f_name':..., ...}
         Cl = Client.query.get(id)
 
         if data['username']:
@@ -66,6 +80,7 @@ class ClientApi(Resource):
         return 200
 
     @staticmethod
+    @jwt_required()
     def delete(self, id):
         c = Client.query.get(id)
         o = Order.query.filter(Order.client_id == id).all()
@@ -92,6 +107,7 @@ class OrderApi(Resource):
                            total = info.total)
 
     @staticmethod
+    @jwt_required()
     def post(self):
         data = requests.get_json(forse=True)
         # data == {f_name:..., ...}
@@ -103,6 +119,7 @@ class OrderApi(Resource):
         return 200
 
     @staticmethod
+    @jwt_required()
     def patch(self, id):
         data = requests.get_json(forse=True)
         # data == {f_name:..., ...}
@@ -118,6 +135,7 @@ class OrderApi(Resource):
         return 200
 
     @staticmethod
+    @jwt_required()
     def delete(self, id):
         o = Order.query.get(id)
         db.session.delete(o)
