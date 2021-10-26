9a882faa79d55ed44ecda106d6b6ccd174b86dbb
Mur_ma <90374636+Murrma@users.noreply.github.com>
2021-10-26 18:18:36 +0300

    Update fastApi.py

--- fastApi.py
+++ fastApi.py
@@ -1,1 +1,46 @@
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel
 
+app = FastAPI()
+
+class Client(BaseModel):
+    id: int
+    username: str
+    password: str
+    f_name: str
+    l_name: str
+class Order(BaseModel):
+    id = int
+    date = str
+    client_id = int
+    total = str
+
+@app.get("/client/items/{id}")
+def get(id: int):
+    if id is None:
+        info = Client(id = id)
+        d = {}
+        for i in info:
+            d[i.id] = [i.username, i.password, i.f_name, i.l_name, ]
+        return d
+
+    else:
+        info = Client#
+        return {'id': info.id,
+                'username': info.username,
+                'password': info.password,
+                'f_name': info.f_name,
+                'l_name': info.l_name}
+
+@app.post('/client/create/')
+def post(info: Client):
+    return info
+
+@app.patch('/client/change/{id}')
+def patch():
+    pass
+
+@app.delete('/client/delete/{id}')
+def delete():
+    pass
