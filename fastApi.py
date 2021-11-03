016fa2a7abd132f8ac391b8e4d8ad1340052f297
Mur_ma <90374636+Murrma@users.noreply.github.com>
2021-11-03 08:14:07 +0300

    Update fastApi.py

--- fastApi.py
+++ fastApi.py
@@ -1,32 +1,22 @@
-from typing import Optional
 from fastapi import FastAPI
-from pydantic import BaseModel
+from ClientModel import Client
+from OrderModel import Order
+from flask_sqlalchemy import SQLAlchemy
 
 app = FastAPI()
+db = SQLAlchemy(app)
 
-class Client(BaseModel):
-    id: int
-    username: str
-    password: str
-    f_name: str
-    l_name: str
-class Order(BaseModel):
-    id: int
-    date: str
-    client_id: int
-    total: str
-
-@app.get("/client/items/{id}")
-def get(id: int):
-    if id is None:
-        info = Client(id = id)
+@app.get("/client/items/{c_id}")
+def get(c_id: int):
+    if c_id is None:
+        info = Client.query.all()
         d = {}
         for i in info:
-            d[i.id] = [i.username, i.password, i.f_name, i.l_name]
+            d[i.id] = [i.username, i.password, i.f_name, i.l_name, ]
         return d
 
     else:
-        info = Client#
+        info = Client.query.get(c_id)
         return {'id': info.id,
                 'username': info.username,
                 'password': info.password,
@@ -35,12 +25,34 @@ def get(id: int):
 
 @app.post('/client/create/')
 def post(info: Client):
-    return info
+    db.session.add(Client(username=info['username'],
+                          password=info['password'],
+                          f_name=info['f_name'],
+                          l_name=info['l_name']))
+    db.session.commit()
+    return 200
 
-@app.patch('/client/change/{id}')
-def patch():
-    pass
+@app.patch('/client/change/{c_id}')
+def patch(data: Client):
+    Cl = Client.query.get(c_id)
+
+    if data['username']:
+        Cl.username = data['username']
+    if data['password']:
+        Cl.password = data['password']
+    if data['f_name']:
+        Cl.f_name = data['f_name']
+    if data['l_name']:
+        Cl.l_name = data['l_name']
+    db.session.commit()
+    return 200
 
 @app.delete('/client/delete/{id}')
 def delete():
-    pass
+    c = Client.query.get(id)
+    o = Order.query.filter(Order.client_id == id).all()
+    db.session.delete(c)
+    for i in o:
+        db.session.delete(i)
+    db.session.commit()
+    return 200
